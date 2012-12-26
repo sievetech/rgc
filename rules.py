0e25fd19b5132889283c8e5ae280a3f0918399a2
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-12-26 15:26:12 -0200

    Add better comments.

--- rules.py
+++ rules.py
@@ -6,20 +6,29 @@ class RuleDoesNotExistError(Exception):
         self.value = rulename
 
     def __str__(self):
-        return 'rule "{0}" is not implemented'.format(self.value)
+        return 'rule "{0}" is not implemented.'.format(self.value)
 
     def __unicode__(self):
         return str(self).decode('utf-8')
-    
+
 
 class RuleSet(object):
+    """
+    Implements each rule that is applied to a cloudfile object.
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        Accepts arguments in the form rulename=param.
+        """
+
         self.rules = kwargs 
 
     def apply(self, obj):
         """
+        Applies and ANDs each rule received as kwargs on object instantiation.
         obj: A file inside a container (cloudfiles.Object)
+        retval: the AND of each rule applied to obj
         """
         ret_val = False
         for rulename, arg in self.rules.iteritems():
@@ -29,6 +38,6 @@ class RuleSet(object):
                 raise RuleDoesNotExistError(rulename)
             else:
                 ret_val &= rule(obj, arg)
-            
+
         return ret_val
 
