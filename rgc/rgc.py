8eb9b2abfebf598e0464c090d18d1c8537139896
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-12-27 18:59:29 -0200

    Add collect function.

--- rgc/rgc.py
+++ rgc/rgc.py
@@ -1,10 +1,23 @@
 #coding: utf-8
-import cloudfiles
 import os
 
-
-def collect(rule=None, container=None, dryrun=None):
-    cloud = cloudfiles.get_connection(os.environ['user'], os.environ['key'])
+import cloudfiles
 
 
-    print  "ok"
+def collect(rule, container='', dryrun=False):
+    conn = cloudfiles.get_connection(os.environ['user'], os.environ['key'])
+    if container:
+        containers = [conn.get_container(container)]
+    else:
+        containers = conn.get_all_containers()
+
+    deleted = []
+    for cont in containers:
+        for obj in cont.get_objects():
+            if rule.apply(obj):
+                if not dryrun:
+                    cont.delete_object(obj.name)
+                deleted.append(obj.name)
+
+    return deleted
+
