c89ca11062e70bce41a0438a238f5b0873c1ac03
Dalton Barreto <daltonmatos@sieve.com.br>
2013-03-18 11:35:08 -0300

    Iterate all objects in a container

--- rgc/rgc.py
+++ rgc/rgc.py
@@ -5,6 +5,17 @@ import cloudfiles
 from clint.textui import progress
 
 
+def _process(cont, deleted, dryrun, objects, rule):
+    for obj in progress.bar(objects, label="Removing Objects"):
+        if rule.apply(obj):
+            if not dryrun:
+                try:
+                    cont.delete_object(obj.name)
+                except Exception as e:
+                    print e
+            deleted.append(obj.name)
+
+
 def collect(user, key, rule, container='', dryrun=False):
     """
     Connects to rackspace with the user and the key and crawls every container
@@ -25,13 +36,13 @@ def collect(user, key, rule, container='', dryrun=False):
     else:
         containers = conn.get_all_containers()
 
-    deleted = []
     for cont in containers:
-        for obj in progress.bar(cont.get_objects(), label="Removing Objects"):
-            if rule.apply(obj):
-                if not dryrun:
-                    cont.delete_object(obj.name)
-                deleted.append(obj.name)
+        objects = cont.get_objects(marker="")
+        while objects:
+            deleted = []
+            _process(cont, deleted, dryrun, objects, rule)
+            objects = cont.get_objects(marker=objects[-1])
+            print "Removed {0} objects".format(len(deleted))
 
     return deleted
 
